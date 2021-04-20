c71aa10cbbb7f47a77052bb7749a4cab5f9a2f30
Abinasshk <abinasshkanesh@hotmail.com>
2021-04-19 20:39:11 -0400

    galerie  ajout animation

--- category-projet.php
+++ category-projet.php
@@ -15,26 +15,18 @@ get_header();
 		<?php if ( have_posts() ) : ?>
 
 			<!-- <header class="page-header"> -->
-			<section classe = "galerie">
+			
 				<?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
-
-			<?php
+			<section class="galerie">
+			<?php 
 			/* Start the Loop */
 			while ( have_posts() ) :
-				the_post(); ?>
-				<div>
-				<?php the_post_thumbnail('thumbnail'); ?>
-				<div class = "info">
-				<h2></h2>
-				<p> description </p>
-				<a href = ""></a>
-				</div>
-
-			
+				the_post();
+				get_template_part( 'template-parts/content', 'galerie' ); ?>
 			<?php endwhile; ?>
 				</section>
 
